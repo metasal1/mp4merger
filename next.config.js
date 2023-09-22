7cbdc312ac0d98c8f642c65bddc7e5494751986b
Salim Karim <imsjk8@gmail.com>
2023-09-22 16:38:52 +1000

    v3

--- next.config.js
+++ next.config.js
@@ -3,15 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
-}
+module.exports = nextConfig
