c69f96f2c929767d8e8bb6ae1c9d25113bda4192
Salim Karim <imsjk8@gmail.com>
2023-09-22 17:00:09 +1000

    works

--- pages/api/merge.js
+++ pages/api/merge.js
@@ -4,7 +4,38 @@ const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 // Set the FFmpeg path for fluent-ffmpeg
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-export default function handler(req, res) {
-  const a = ffmpeg().input('./public/heytoly.mp4').videoCodec('copy').input('./public/heytoly.mp3').audioCodec('aac').save('output.mp4');
-  res.status(200).json({ name: 'John Doe', a });
+export default async function handler(req, res) {
+
+  const answer = req.query.answer;
+  console.log('answer', answer);
+  const request = await fetch('https://play.ht/api/v2/tts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.PLAYHT_TOKEN}`,
+      'X-User-Id': process.env.PLAYHT_USER_ID
+    },
+    body: JSON.stringify({
+      "text": answer,
+      "voice": "s3://voice-cloning-zero-shot/86cc02af-4340-4b54-8fcb-6a870b6c9bd2/toly/manifest.json",
+    })
+  });
+  const response = await request.text();
+  const regex = /"url":"(https:\/\/[^\"]+)"/;
+  const match = response.match(regex);
+  var url;
+  if (match && match[1]) {
+    url = match[1];
+    console.log('Extracted URL:', url);
+    const filename = `output-${Date.now()}.mp4`;
+    ffmpeg().input('./public/heytoly.mp4').videoCodec('copy').input(url).audioCodec('aac').save(`./public/${filename}`)
+      .on('end', () => { res.status(200).json({ downloadLink: `/${filename}` }) })
+      .on('error', (err) => {
+        console.error("Error:", err);
+        res.status(500).json({ error: 'Failed to process video' });
+      });
+  } else {
+    console.log('URL not found');
+  }
+
 }
